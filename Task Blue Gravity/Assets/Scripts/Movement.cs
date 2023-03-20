133ec09dd51f63c71708244a61de62955ff93292
matheuscarreirao <matheus91cc@gmail.com>
2023-03-20 17:49:52 -0300

    - adding movement to character

--- Task Blue Gravity/Assets/Scripts/Movement.cs
+++ Task Blue Gravity/Assets/Scripts/Movement.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 1;
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector2 movement = Vector2.zero;
+
+    private Animator myAnimator;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,46 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Player inputs to move character
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        movement = new Vector2(horizontalInput, verticalInput);
+        FlipPlayer();
+        AnimationUpdate();
+
+        Debug.Log("Horizontal: " + horizontalInput + " | Vertical: " +verticalInput);
+
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    // Check with direction it player is going to flip player 
+    private void FlipPlayer()
+    {
+        Vector3 newScale = transform.localScale;
+
+        if (horizontalInput < 0 )
+        {
+            newScale.x = 1f;
+        }
+        else if(horizontalInput > 0)
+        {
+            newScale.x = -1f;
+        }
+
+        transform.localScale = newScale;
+    }
+
+    //Update character animation
+    private void AnimationUpdate()
+    {      
+        //Use always a positive value to change the animation parameter
+        if (verticalInput < 0 || verticalInput > 0)
+        {
+            myAnimator.SetFloat("moveSpeed", Mathf.Abs(verticalInput));
+        }
+        else
+        {
+            myAnimator.SetFloat("moveSpeed", Mathf.Abs(horizontalInput));
+        }       
     }
 }
